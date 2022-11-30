c91b89969614d29d71b0519730f2da4c8631f7b0
Roman Ivanov <romka150495@gmail.com>
2022-11-30 16:28:44 +0300

    create

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Введите предложение (разделите слова пробелами) : ");
+            ShowSplittedText(SplitText(ReadLine()));
+            ReadLine();
+        }
+
+        /// <summary>
+        /// Метод, выводящий в консоль результат разделения на слова
+        /// </summary>
+        /// <param name="someText"></param>
+        static void ShowSplittedText(string[] someWords)
+        {
+            foreach (var word in someWords)
+            {
+                WriteLine($" {word} ");
+            }
+        }
+
+        /// <summary>
+        /// Метод, разделяющий введённый текст на слова
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string[] SplitText(string enteredText)
+        {
+            return enteredText.Split(' ');
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Введите предложение (разделите слова пробелами) : ");
+            WriteLine(Reverse(ReadLine()));
+            ReadLine();
+        }
+
+        /// <summary>
+        /// Метод, разворачивабщий порядок слов в предложении
+        /// </summary>
+        /// <param name="inputPhrase"></param>
+        static string Reverse(string inputPhrase)
+        {
+            string resultString=null;
+            for (int i = SplitText(inputPhrase).Length-1; i >=0; i--)
+            {
+                resultString += SplitText(inputPhrase)[i] + " ";
+            }
+            return resultString;
+        }
+
+        /// <summary>
+        /// Метод, разделяющий введённый текст на слова
+        /// </summary>
+        /// <param name="enteredText"></param>
+        /// <returns></returns>
+        static string[] SplitText(string enteredText)
+        {
+            return enteredText.Split(' ');
+        }
+    }
+}
